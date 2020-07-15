a51c52e9385fcdcaa4a1bd11b57e6aefe0b5068c
Prateek Chawla <prateek.chawla98@gmail.com>
2020-07-16 00:52:14 +0530

    Modify Urls

--- src/urls-sample.js
+++ src/urls-sample.js
@@ -2,6 +2,8 @@ export const FIREBASE_BASE_URL = "<INSERT_YOUR_FIREBASE_URL_HERE>";
 
 export const FIREBASE_INGREDIENTS_URL = `${FIREBASE_BASE_URL}/ingredients.json`;
 
-export const FIREBASE_SIGN_UP_URL = "<INSERT_YOUR_FIREBASE_SIGN_UP_URL_HERE>";
+const FIREBASE_WEB_API_KEY = "<INSERT_YOUR_WEB_API_KEY_HERE>";
 
-export const FIREBASE_SIGN_IN_URL = "<INSERT_YOUR_FIREBASE_SIGN_IN_URL_HERE>";
+export const FIREBASE_SIGN_UP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_WEB_API_KEY}`;
+
+export const FIREBASE_SIGN_IN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_WEB_API_KEY}`;
